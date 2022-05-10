c5cdd1cb35a2497aff561bbfc68453ce3957b532
Beyhan <beyhan.taylan@hotmail.com>
2022-05-10 19:10:09 -0400

    commit tree 0e0abf71811916ea2e8ab61a2b61957d8dbd3e5b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// const superbowlWin = (records) => {
+//     const result = records.find((record) => record.result === "W")
+  
+//     if (result) {
+//         return result.year
+//     }
+//   }
+
+
+  function superbowlWin(record){
+      let result = record.find(res => res.result === "W")
+      if (result){
+          return result.year
+      }
+  }
